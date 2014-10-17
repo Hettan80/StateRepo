380fbe8f92e017dd5bd67937e75545f9600168a6
LordCopper <acoppero@gmail.com>
2014-10-17 10:56:34 +0200

    Interface

---
+++ StateSolution/StateLib/IClock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateLib
+{
+    interface IClock
+    {
+         YearState YearState { get; set; }
+         MonthState MonthState { get; set; }
+         DayState DayState { get; set; }
+    }
+}
